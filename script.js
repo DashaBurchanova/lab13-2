e2d1edb7fe0ecbe1d5be694f992df406740d2758
DashaBurchanova <79722680+DashaBurchanova@users.noreply.github.com>
2021-10-25 23:24:09 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$('.slider').slick({
+arrows: false,
+dots: true,
+speed: 1000,
+autoplay: true,
+autoplaySpeed: 2000,
+centerMode: true,
+adaptiveHeight: true,
+
+});
+});
